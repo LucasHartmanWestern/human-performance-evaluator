717de6dbd7a8f1787d272fec508fc029fe0d2d73
LucasHartman <lucas.hartman@kognitive.ca>
2023-08-31 16:12:07 -0400

    Add darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,30 @@ export class AppComponent {
   formSubmission: any;
 
   ngOnInit(): void {
+    if (localStorage.getItem('displayMode') == 'dark') {
+      document.querySelector('body')?.setAttribute('class', 'dark');
+      document.getElementById('darkmode-toggle')?.setAttribute('checked', 'true');
+    }
   }
 
   formSubmit(): void {
     this.formData = localStorage.getItem('userInfo');
+  }
+
+  toggleMode(): void {
+    let body = document.querySelector('body');
+
+    let mode = localStorage.getItem('displayMode');
+    if (!mode) {
+      let bodyClass = body?.getAttribute('class');
+      mode = bodyClass || 'light';
+      mode = mode == 'dark' ? 'light' : 'dark'
+      localStorage.setItem('displayMode', mode);
+    } else {
+      mode = mode == 'dark' ? 'light' : 'dark'
+      localStorage.setItem('displayMode', mode);
+    }
+
+    body?.setAttribute('class', mode);
   }
 }
