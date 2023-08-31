bf9b73ef1cff1b730f44508462b2ec15481bc445
LucasHartman <lucas.hartman@kognitive.ca>
2023-08-30 21:33:01 -0400

    Create template game window

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,20 +10,47 @@ export class GameComponent {
 
   constructor(private gameEndpointsService: GameEndpointsService) { }
 
-  image: { file: string, posX: string, posY: string, width: string, height: string } | undefined;
+  image: { file: string, posX: number, posY: number, width: number, height: number } | undefined;
 
   // Get users data
   ngOnInit(): void {
     this.gameEndpointsService.getFirstImage().subscribe(res => {
       this.image = res;
+      this.moveButton();
     }, error => {
       this.image = {
         file: '',
-        posX: '',
-        posY: '',
-        width: '',
-        height: ''
+        posX: 155,
+        posY: 202,
+        width: 24,
+        height: 24
       }
+      this.moveButton();
     });
+  }
+
+  moveButton(): void {
+    let windowContainer = document.getElementById('window');
+    let img = document.querySelector('img');
+    windowContainer?.setAttribute('style', `height: ${img?.offsetHeight}px; width: ${img?.offsetWidth}px;`);
+
+    let button = document.getElementById('foundButton');
+    button?.setAttribute('style', `width: ${this.image?.width}px; height: ${this.image?.height}px; top: ${this.image?.posY}px; left: ${this.image?.posX}px;`)
+  }
+
+  found(): void {
+    console.log('FOUND');
+  }
+
+  mistake(): void {
+    console.log('MISTAKE');
+  }
+
+  begin(): void {
+    console.log('BEGIN');
+  }
+
+  end(): void {
+    console.log('END');
   }
 }
